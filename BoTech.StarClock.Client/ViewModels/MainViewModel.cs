74a914ac211af5bd5391174c3ac3a1f634511176
BoTech-Development <fteetz@outlook.de>
2025-08-24 11:07:31 +0200

    Initial commit

---
+++ BoTech.StarClock.Client/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using BoTech.StarClock.Client.Helper;
+
+namespace BoTech.StarClock.Client.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    
+
+    public MainViewModel()
+    {
+      /*  BluetoothClientHelper client = new BluetoothClientHelper();
+        Thread clientThread = new Thread(async () =>
+        {
+            await client.DiscoverAndConnectAsync();
+            Thread readHandler = new Thread(() =>
+            {
+                while (true) client.ReceiveData();
+            });
+            Thread writeHandler = new Thread(() =>
+            {
+                string? input = null;
+                while (true)
+                {
+                    input = Console.ReadLine();
+                    if (input != null) client.SendData(input);
+                }
+            });
+            writeHandler.Start();
+            readHandler.Start();
+        });
+        clientThread.Start();*/
+    }
+}
---
+++ BoTech.StarClock.Client/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace BoTech.StarClock.Client.ViewModels;
+
+public abstract class ViewModelBase : ReactiveObject
+{
+}
